5bebb37d1b8e8170ee388302d6d4ffdaf1e0d129
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-03 00:40:25 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,19 @@
-const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+const express = require('express')
+app.get("/sol1", function(req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr = [1, 2, 3, 5, 6, 7]
 
-const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+    let sumOfGivenArray = 0
+    for (leti = 0; i < arr.length; i++) {
+        sumOfGivenArray = sumOfGivenArray + arr[i]
+    }
+
+    let sumofAllNumbers = (arr.length * (arr.length + 1)) / 2
+
+    let missingNumber = sumofAllNumbers - sumOfGivenArray
+
+    console.log("The missing Number is: " + missingNumber)
+
+    res.send({ data: missingNumber });
 });
-
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-
-module.exports = router;
-// adding this comment for no reason
