2283933805271e80255a63f3f41584d1fe495ba6
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-03 00:23:17 +0530

    Updates

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,14 +4,26 @@ const lodashFunctions = require('lodash');
 const router = express.Router();
 
 router.get('/month', function(req, res) {
+
+    // Probelm 4 - A
     const allMonth = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
     console.log(lodashFunctions.chunk(allMonth, 3))
 
-    const arrUnion = [1, 1, 1, 1, 1, 2, 3, 4, 3, 2, 3, 4, 3, 4, 3, 4]
-    console.log(lodashFunctions.union(arrUnion))
+    //Problem 4 - B
 
     const lastNineOddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17]
     console.log(lodashFunctions.tail(lastNineOddNumbers))
+
+    //Problem 4 - C
+
+    let a = [1, 2, 3, 3]
+    let b = [9, 3, 6, 3]
+    let c = [5, 3, 1]
+    let d = [3, 6, 8, 6]
+    let e = [1, 5, 8, 3, 4, 7, 3]
+    console.log(lodashFunctions.union(a, b, c, d, e))
+
+    //Problem 4 - D  
 
     const keyArray = [
         ["horror", "The Shining"],
@@ -20,8 +32,8 @@ router.get('/month', function(req, res) {
         ["fantacy", "Labyrinth"]
 
     ]
-
     console.log(lodashFunctions.fromPairs(keyArray))
+
 
     res.send("The solution for problem statement number 4 of nodejs module assignment.")
 });
