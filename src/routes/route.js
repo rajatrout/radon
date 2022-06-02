bed449d3e85d070826bc8641ca57abcce87a2547
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-03 01:00:37 +0530

    Updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,8 @@
 const express = require('express')
-app.get("/sol1", function(req, res) {
+
+const router = express.Router();
+
+router.get("/sol1", function(req, res) {
     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
     let arr = [1, 2, 3, 5, 6, 7]
 
