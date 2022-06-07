515c6be8500450833e03706fb330fb8b6ac185d0
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-07 12:56:54 +0530

    Updated

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,23 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true
     },
-    sales: {type: Number, default: 10}
+    bookPrize: {
+        indianPrize: String,
+        europeanPrize: String
+    },
+    year: {
+        type: Number,
+        default: 2021
+    },
+    tags: ["Finction", "Non-Fiction", "Novel", "Narative", "Science-Fiction", "Mystery", "Fantasy"],
+    authorName: String,
+    totalPages: Number,
+    stockAvailable: Boolean
+
 }, { timestamps: true });
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model('User', bookSchema)
