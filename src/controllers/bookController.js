a5fc43b8453eada394fa479c832c9957d529b862
rajatrout <rajatrout@Rajats-MacBook-Air.local>
2022-06-10 16:13:05 +0530

    Updated

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,30 +2,55 @@ const bookModel = require("../models/bookModel")
 const PublisherModel = require("../models/publisherModel.js")
 const AuthorModel = require("../models/authorModel.js")
 
+// const createBook = async function(req, res) {
+
+//     let book = req.body
+
+//     let authorId = await AuthorModel.find().select({ _id: 1 })
+//     console.log(authorId)
+//     let authorIdArr = authorId.map((obj) => { return obj._id.toString() })
+//     console.log(authorIdArr)
+
+//     let publisheId = await PublisherModel.find().select({ _id: 1 })
+//     let publishIdArr = publisheId.map((obj) => { return obj._id.toString() })
+
+//     if (book.authorId != undefined) {
+//         if (book.publisherId != undefined) {
+//             if (authorIdArr.includes(book.authorId)) {
+//                 if (publishIdArr.includes(book.publisherId)) {
+//                     let bookCreated = await bookModel.create(book)
+//                     return res.send({ data: bookCreated })
+//                 }
+//                 return res.send({ data: "Invalid Publisher Id" })
+//             }
+//             return res.send({ data: "Invalid Author Id" })
+//         }
+//         return res.send({ data: "Missing Publisher Id" })
+//     }
+//     return res.send({ data: "Missing Author Id" })
+// }
+
+
 const createBook = async function(req, res) {
+    const book = req.body
+    const author = req.body.authorId
 
-    let book = req.body
+    const publisher = req.body.publisherId
 
-    let authorId = await AuthorModel.find().select({ _id: 1 })
-    let authorIdArr = authorId.map((obj) => { return obj._id.toString() })
+    if (!author || !publisher) {
+        return res.send({ msg: "The authorId and publisherId are required" })
+    } else {
+        const m = await AuthorModel.findById(author)
 
-    let publisheId = await PublisherModel.find().select({ _id: 1 })
-    let publishIdArr = publisheId.map((obj) => { return obj._id.toString() })
+        const n = await PublisherModel.findById(publisher)
 
-    if (book.authorId != undefined) {
-        if (book.publisherId != undefined) {
-            if (authorIdArr.includes(book.authorId)) {
-                if (publishIdArr.includes(book.publisherId)) {
-                    let bookCreated = await bookModel.create(book)
-                    return res.send({ data: bookCreated })
-                }
-                return res.send({ data: "Invalid Publisher Id" })
-            }
-            return res.send({ data: "Invalid Author Id" })
+        if (!m || !n) {
+            return res.send({ msg: "AuthorID or PublisherId is not present" })
+        } else {
+            let a = await bookModel.create(book)
+            return res.send({ msg: a, status: true })
         }
-        return res.send({ data: "Missing Publisher Id" })
     }
-    return res.send({ data: "Missing Author Id" })
 }
 
 
